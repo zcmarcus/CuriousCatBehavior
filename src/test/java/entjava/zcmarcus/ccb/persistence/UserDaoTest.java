06a2b417be9cb4bad3b1b7a2fc1e07a581da2f16
zcmarcus <zcm.box@gmail.com>
2020-02-15 12:45:04 -0600

    Add skeleton for UserDao JUnit test suite

---
+++ src/test/java/entjava/zcmarcus/ccb/persistence/UserDaoTest.java
@@ -0,0 +1,41 @@
+package entjava.zcmarcus.ccb.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getByUserName() {
+    }
+
+    @Test
+    void getByProperty() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/resources/clean_database.sql
@@ -0,0 +1,7 @@
+DELETE from user;
+INSERT INTO user VALUES (1,'jpublic','secretpw1','jpublic@wisc.edu','Public', 'John', '2020-02-05')
+                        ,(2,'kmalone','secretpw2','kmalone@jazz.com','Malone', 'Karl', '2020-02-06')
+                        ,(3,'dduany','secretpw3','dduany@wisc.edu','Duany', 'Duany', '2020-02-07')
+                        ,(4,'hcoulson','secretpw4','hcoulson@hotmail.com','Coulson', 'Hannah', '2020-02-08')
+                        ,(5,'szawistowski','secretpw5','sz01@wisc.edu','Zawistowski', 'Samantha', '2020-02-09')
+                        ,(6,'bcurry','secretpw6','bcurry@hotmail.com','Curry', 'Barney', '2020-02-10')
